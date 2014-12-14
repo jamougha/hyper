3a80b3725c9ebad2964934e5b3ba1fcc1de15d36
James Moughan <jamougha@gmail.com>
2014-12-14 00:24:30 +0000

    use FromStr for the referer header, added some more tests

--- src/header/common/referer.rs
+++ src/header/common/referer.rs
@@ -1,6 +1,6 @@
 use header::{Header, HeaderFormat};
-use std::str::from_utf8;
-use std::fmt::{mod, Show, Formatter};
+use std::str::{from_utf8, FromStr};
+use std::fmt::{mod, Show, Formatter, Error};
 use url::format::{PathFormatter};
 use url::SchemeData::{NonRelative, Relative};
 use Url;
@@ -28,18 +28,24 @@ impl Header for Referer {
         if raw.len() != 1 {
             None
         } else {
-            let header = from_utf8(raw[0].as_slice())
-                           .map(|h| h.trim());
-
-            match header {
-                None => None,
-                Some("about:blank") => Some(Referer::Blank),
-                Some(s) => Url::parse(s).ok()
-                               .map(|url| Referer::RefererUrl(url))
-            }
+            from_utf8(raw[0].as_slice())
+                .map(|h| h.trim())
+                .and_then(|s| FromStr::from_str(s))
         }
     }
 
+}
+
+impl FromStr for Referer {
+    fn from_str(s: &str) -> Option<Referer> {
+        if s == "about:blank" {
+            Some(Referer::Blank)
+        } else {
+            Url::parse(s).ok()
+                .map(|url| Referer::RefererUrl(url))
+        }
+
+    }
 }
 
 impl HeaderFormat for Referer {
@@ -56,7 +62,7 @@ impl HeaderFormat for Referer {
                 try!(fmt.write(b":"));
 
                 match url.scheme_data {
-                    NonRelative(_) => url.scheme_data.fmt(fmt),
+                    NonRelative(_) => try!(url.scheme_data.fmt(fmt)),
                     Relative(ref data) => {
                         try!(fmt.write(b"//"));
 
@@ -66,15 +72,32 @@ impl HeaderFormat for Referer {
                             try!(write!(fmt, ":{}", port));
                         }
 
-                        PathFormatter {
+                        try!(PathFormatter {
                             path: data.path.as_slice()
-                        }.fmt(fmt)
+                        }.fmt(fmt));
                     }
-
                 }
+
+                if let Some(ref query) = url.query {
+                    try!(fmt.write(b"?"));
+                    try!(fmt.write(query.as_bytes()));
+                }
+
+                Ok(())
             }
         } 
 
+    }
+}
+
+
+// This is just testing boilerplate.
+struct RefererFormatter<'a>(pub &'a Referer);
+
+impl<'a> Show for RefererFormatter<'a> {
+    fn fmt(&self, fmt: &mut Formatter) -> Result<(), Error> {
+        let RefererFormatter(referer) = *self;
+        referer.fmt_header(fmt)
     }
 }
 
@@ -82,12 +105,37 @@ impl HeaderFormat for Referer {
 fn test_parse_absolute_uri() {
     let header: Vec<u8> = "http://www.example.com/foo/bar?baz#fragment"
                             .as_bytes().iter().map(|x| *x).collect();
-    let referer: Option<Referer> = Header::parse_header(&[header]);
+    let referer: Referer = Header::parse_header(&[header]).unwrap();
 
-    if let Some(Referer::RefererUrl(url)) = referer {
+    if let Referer::RefererUrl(ref url) = referer {
+
         assert_eq!("http", url.scheme);
         let url_string = "//www.example.com/foo/bar";
         assert_eq!(url_string, format!("{}", url.scheme_data));
+        assert_eq!(Some("baz".to_string()), url.query);
+        
+        assert_eq!("http://www.example.com/foo/bar?baz", 
+                    format!("{}", RefererFormatter(&referer)));
+    } else {
+        panic!();
     }
 }
 
+#[test]
+fn test_remove_userinfo() {
+    let header: Vec<u8> = "http://userinfo:foo@host:9/foo.bar"
+                            .as_bytes().iter().map(|x| *x).collect();
+    let referer: Referer = Header::parse_header(&[header]).unwrap();
+
+    assert_eq!("http://host:9/foo.bar".to_string(), 
+                format!("{}", RefererFormatter(&referer)));
+}
+
+
+#[test]
+fn test_parse_relative_uri() {
+    let header: Vec<u8> = "foo/bar?baz"
+                            .as_bytes().iter().map(|x| *x).collect();
+    let referer: Option<Referer> = Header::parse_header(&[header]);
+    referer.expect("We should be using a URI here, not a URL.");
+}
